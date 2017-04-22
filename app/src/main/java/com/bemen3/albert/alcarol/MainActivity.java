bb61aa6a543b365df8190c332f9e06e40b102579
Albert Cadena <albert.cad.rub@gmail.com>
2017-04-22 18:28:22 +0200

    Initial commit

---
+++ app/src/main/java/com/bemen3/albert/alcarol/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bemen3.albert.alcarol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
